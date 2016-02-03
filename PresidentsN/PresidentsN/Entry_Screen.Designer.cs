8ff173f5e95ed5bb82b1a7b1de34fc9061b6b8b5
David Wilson <davidwilson56@gmail.com>
2016-02-02 20:19:01 -0500

    First project upload with entry screen which launches client screen

---
+++ PresidentsN/PresidentsN/Entry_Screen.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PresidentsN
+{
+    partial class EntryScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OptionsBox = new System.Windows.Forms.GroupBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.Heading = new System.Windows.Forms.Label();
+            this.Heading2 = new System.Windows.Forms.Label();
+            this.OptionsBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OptionsBox
+            // 
+            this.OptionsBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OptionsBox.AutoSize = true;
+            this.OptionsBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.OptionsBox.Controls.Add(this.StartBtn);
+            this.OptionsBox.Location = new System.Drawing.Point(117, 83);
+            this.OptionsBox.Margin = new System.Windows.Forms.Padding(2);
+            this.OptionsBox.Name = "OptionsBox";
+            this.OptionsBox.Padding = new System.Windows.Forms.Padding(2);
+            this.OptionsBox.Size = new System.Drawing.Size(144, 66);
+            this.OptionsBox.TabIndex = 2;
+            this.OptionsBox.TabStop = false;
+            this.OptionsBox.Text = "Game Options";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(12, 28);
+            this.StartBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(128, 21);
+            this.StartBtn.TabIndex = 2;
+            this.StartBtn.Text = "Play Online";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // Heading
+            // 
+            this.Heading.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Heading.AutoSize = true;
+            this.Heading.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Heading.Location = new System.Drawing.Point(1, 0);
+            this.Heading.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(377, 37);
+            this.Heading.TabIndex = 3;
+            this.Heading.Text = "Welcome To Presidents";
+            this.Heading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Heading2
+            // 
+            this.Heading2.AutoSize = true;
+            this.Heading2.Location = new System.Drawing.Point(114, 51);
+            this.Heading2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Heading2.Name = "Heading2";
+            this.Heading2.Size = new System.Drawing.Size(133, 13);
+            this.Heading2.TabIndex = 4;
+            this.Heading2.Text = "Select An Option To Begin";
+            this.Heading2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EntryScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(384, 189);
+            this.Controls.Add(this.Heading2);
+            this.Controls.Add(this.Heading);
+            this.Controls.Add(this.OptionsBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 227);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 227);
+            this.Name = "EntryScreen";
+            this.Text = "EntryScreen";
+            this.OptionsBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox OptionsBox;
+        private System.Windows.Forms.Label Heading;
+        private System.Windows.Forms.Label Heading2;
+        private System.Windows.Forms.Button StartBtn;
+    }
+}
+
---
+++ PresidentsN/PresidentsN/Entry_Screen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresidentsN
+{
+    public partial class EntryScreen : Form
+    {
+        public EntryScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            ClientForm C1 = new ClientForm();
+            this.Hide();
+            C1.ShowDialog();
+        }
+    }
+}
